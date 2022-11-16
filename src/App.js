988723c5fbff16ba113e24db5e097fcaddc9f81e
Alireza <alireza215487@gmail.com>
2022-11-16 20:25:26 +0330

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { Redirect, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import Home from './Home.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <BrowserRouter>
+              <layout>
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route path="*" element={<Home />} />
+                </Routes>
+              </layout>
+      </BrowserRouter>
+  )
 }
 
 export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const Home = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+              <img src="/MedusaLogo.png" alt="logo" width="500" height="auto"/>
+                  <p>
+                    Medusa Laptop
+                </p>
+                <p>Soon...</p>
+            </header>
+        </div>
+    );
+};
+
+export default Home;
